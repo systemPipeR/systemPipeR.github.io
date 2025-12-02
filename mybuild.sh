c920e7cd7bbf73e46f07b8fb54803087906b0c43
tgirke <tgirke@citrus.ucr.edu>
2025-12-01 19:51:20 -0800

    build instructions

---
+++ mybuild.sh
@@ -0,0 +1,22 @@
+###################################################################
+## Instruction how to build site locally and then push to github ##
+###################################################################
+## Thomas added on 01-Dec-2025
+
+## One necessary fix (one time setup change) was to ignore jekyll in build, which gave errors
+## under github actions. Adding a .nojekyll file fixed this.
+git checkout main # just in case
+touch .nojekyll # this file instructs to ignor jekyll
+git add .nojekyll
+git commit -am “no_render” # Tell github action not to render.
+git push
+
+## Deploy site from local
+git checkout main # just in case
+hugo --verbose --cleanDestinationDir # build site. This creates rendered html files under `public/` 
+git add . # needs to be done to account for changes in `public/`
+git commit -am "no_render" # tell github action not to render. 
+git push origin main
+git push origin `git subtree split --prefix public`:gh-pages --force # use only to resolve possible error in push
+# git subtree push --prefix public origin gh-pages # alternative not used
+
